d8b198d2ee470b422cdc7f15b1234bc8146d5cc8
iriduan900 <iriduan900@gmail.com>
2026-02-22 23:51:37 +0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+window.LOCKEDTUBE_CONFIG = {
+  apiKey: "AIzaSyCBfA_anTR0ZolneATQnH1NkP2Nmj9fw8g",
+  allowedHandles: ["MrBeast"],
+  maxPerChannel: 24,
+  hideShorts: true,
+
+  // ✅ remove anything below 1 minute
+  minDurationSeconds: 60
+};
